37a52fbae0c7e794e3b9b035a05fae836e6822e0
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2019-08-02 18:06:04 -0300

    LPS-97844 Adapt front end templates to handle portal versions 7.0, 7.1 and 7.2

--- modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/java/form/field/__className__DDMFormFieldType.java
+++ modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/java/form/field/__className__DDMFormFieldType.java
@@ -14,13 +14,14 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 #if (${liferayVersion.startsWith("7.0")})
 		"ddm.form.field.type.display.order:Integer=9",
-#elseif (${liferayVersion.startsWith("7.1")})
+#else if (${liferayVersion.startsWith("7.1")} || ${liferayVersion.startsWith("7.2")})
 		"ddm.form.field.type.description=${artifactId}-description",
 		"ddm.form.field.type.display.order:Integer=10",
-#end
+#if (!${liferayVersion.startsWith("7.2")})
 		"ddm.form.field.type.icon=text",
 		"ddm.form.field.type.js.class.name=Liferay.DDM.Field.${className}",
 		"ddm.form.field.type.js.module=${artifactId}-form-field",
+#end
 		"ddm.form.field.type.label=${artifactId}-label",
 		"ddm.form.field.type.name=${formFieldTypeName}"
 	},
--- modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/__artifactId__.es.js
+++ modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/__artifactId__.es.js
@@ -1,13 +1,44 @@
+#if (${liferayVersion.startsWith("7.2")})
+import '../FieldBase/FieldBase.es';
+import './${artifactId}Register.soy.js';
+import templates from './${artifactId}.soy.js';
+import {Config} from 'metal-state';
+#else
+import templates from './${artifactId}.soy';
+#end
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
-import templates from './${artifactId}.soy';
 
 /**
  * ${className} Component
  */
 class ${className} extends Component {}
 
+#if (${liferayVersion.startsWith("7.2")})
+${className}.STATE = {
+
+	/**
+	 * @default undefined
+	 * @instance
+	 * @memberof Text
+	 * @type {?(string|undefined)}
+	 */
+
+	name: Config.string().required(),
+
+	/**
+	 * @default undefined
+	 * @instance
+	 * @memberof Text
+	 * @type {?(string|undefined)}
+	 */
+
+	value: Config.string().value('')
+}
+
+Soy.register(${className}, templates);
+#else
 // Register component
 Soy.register(${className}, templates, 'render');
 
@@ -18,5 +49,5 @@ if (!window.DDM${className}) {
 }
 
 window.DDM${className}.render = ${className};
-
+#end
 export default ${className};
