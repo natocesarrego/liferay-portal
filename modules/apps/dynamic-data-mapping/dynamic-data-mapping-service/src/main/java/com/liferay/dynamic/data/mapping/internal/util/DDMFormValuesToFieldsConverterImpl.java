f758f2f35b053db4d34af092df04b08a094ca5a1
Renato Rego <renato.rego@liferay.com>
2019-10-14 14:31:00 -0300

    LPS-X Consider locale to convert double values using comma as decimal separator.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/DDMFormValuesToFieldsConverterImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/DDMFormValuesToFieldsConverterImpl.java
@@ -26,6 +26,7 @@ import com.liferay.dynamic.data.mapping.util.DDMFormValuesToFieldsConverter;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -179,8 +180,18 @@ public class DDMFormValuesToFieldsConverterImpl
 		Field ddmField, String type, Value value) {
 
 		for (Locale availableLocales : value.getAvailableLocales()) {
-			Serializable serializable = FieldConstants.getSerializable(
-				type, value.getString(availableLocales));
+			Serializable serializable = null;
+
+			if (type.equals(FieldConstants.DOUBLE) &&
+				value.getString(availableLocales).contains(StringPool.COMMA)) {
+
+				serializable = GetterUtil.getDouble(
+					value.getString(availableLocales), availableLocales);
+			}
+			else {
+				serializable = FieldConstants.getSerializable(
+					type, value.getString(availableLocales));
+			}
 
 			ddmField.addValue(availableLocales, serializable);
 		}
