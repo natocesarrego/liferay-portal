47b1d043040e4437217d06792be6cfc14ac384a7
Marcos <marcos.martins@liferay.com>
2020-03-17 14:50:22 -0300

    LPS-109805 Fixing visibility expression upgrade

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/ExpressionParameterExtractor.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.internal.util;
+
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Marcos Martins
+ */
+public class ExpressionParameterExtractor {
+
+	/**
+	 * It returns an array with the given expression parameters
+	 *
+	 * @param expression Ex: equals('Country', "US")
+	 * @return String[] Ex: ['Country', "US"]
+	 */
+	public static String[] extractParameters(String expression) {
+		List<String> parameterList = Arrays.asList(
+			expression.split(_FUNCTION_STRUCTURE_REGEX));
+
+		Stream<String> parameterStream = parameterList.stream();
+
+		parameterList = parameterStream.filter(
+			parameter -> Validator.isNotNull(parameter)
+		).collect(
+			Collectors.toList()
+		);
+
+		return parameterList.toArray(new String[0]);
+	}
+
+	private static final String _FUNCTION_STRUCTURE_REGEX =
+		"([aA-zZ])*.([(])|,[ ]*|[)]";
+
+}
