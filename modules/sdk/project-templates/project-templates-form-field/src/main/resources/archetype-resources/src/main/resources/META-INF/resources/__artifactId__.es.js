ef2d63606aada620c625cdcefc25f6f9d49dd97f
Gabriel Ibson <gabriel.souza@liferay.com>
2020-03-19 10:52:52 -0300

    LPS-98417 Updating project-template-form-field to support 7.2

--- modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/__artifactId__.es.js
+++ modules/sdk/project-templates/project-templates-form-field/src/main/resources/archetype-resources/src/main/resources/META-INF/resources/__artifactId__.es.js
@@ -1,6 +1,14 @@
+#parse ("definitions.vm")
+#if (${liferayVersion.startsWith("7.2")})
+import 'dynamic-data-mapping-form-field-type/FieldBase/FieldBase.es';
+import './${artifactId}Register.soy.js';
+import {Config} from 'metal-state';
+import templates from './${artifactId}.soy.js';
+#end
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
+#if (!${liferayVersion.startsWith("7.2")})
 import templates from './${artifactId}.soy';
 
 /**
@@ -18,5 +26,48 @@ if (!window.DDM${className}) {
 }
 
 window.DDM${className}.render = ${className};
+#else
+/**
+ * ${className} Component
+ */
+class ${className} extends Component {
 
+    dispatchEvent(event, name, value) {
+        this.emit(name, {
+            fieldInstance: this,
+            originalEvent: event,
+            value
+        });
+    }
+
+    _handleFieldChanged(event) {
+        const {value} = event.target;
+
+        this.setState(
+            {
+                value
+            },
+            () => this.dispatchEvent(event, 'fieldEdited', value)
+        );
+    }
+}
+
+${className}.STATE = {
+
+    name: Config.string().required(),
+
+    predefinedValue: Config.oneOfType([Config.number(), Config.string()]),
+
+    required: Config.bool().value(false),
+
+    showLabel: Config.bool().value(true),
+
+    spritemap: Config.string(),
+
+    value: Config.string().value('')
+}
+
+// Register component
+Soy.register(${className}, templates);
+#end
 export default ${className};
