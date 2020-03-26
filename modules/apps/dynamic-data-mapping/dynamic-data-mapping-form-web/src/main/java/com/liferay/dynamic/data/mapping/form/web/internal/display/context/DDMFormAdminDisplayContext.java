1d39a4673affd2016d98887c1ecbe79b92b7ab0c
Marcos <marcos.martins@liferay.com>
2020-03-26 08:53:12 -0300

    LPS-110391 testing form instance published url on back-end

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
@@ -759,7 +759,7 @@ public class DDMFormAdminDisplayContext {
 	public String getPublishedFormURL(DDMFormInstance formInstance)
 		throws PortalException {
 
-		if (formInstance == null) {
+		if (!isFormPublished(formInstance)) {
 			return StringPool.BLANK;
 		}
 
