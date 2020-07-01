9dcb10efce805a7c5bd4f3da81aa1325d6402445
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-06-30 22:47:42 -0300

    LPS-116301 Use portletNamespace as prefix of id

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/sidebar/Sidebar.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/sidebar/Sidebar.es.js
@@ -57,7 +57,7 @@ export default () => {
 	return (
 		<>
 			<div className="sidebar-backdrop"></div>
-			<div className="open sidebar-container" id="sidebar-reports">
+			<div className="open sidebar-container" id={`${portletNamespace}-sidebar-reports`}>
 				<div className="sidebar sidebar-light">
 					<nav className="component-tbar tbar">
 						<div className="container-fluid">
@@ -105,7 +105,7 @@ export default () => {
 							</div>
 						)}
 
-						{!!Object.entries(summary).length && (
+						{Object.keys(summary).length && (
 							<Summary summary={summary} />
 						)}
 
