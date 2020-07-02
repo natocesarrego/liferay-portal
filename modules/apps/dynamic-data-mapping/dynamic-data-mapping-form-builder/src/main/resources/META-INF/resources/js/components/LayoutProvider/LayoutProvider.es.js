2e7bac33f5e973bc587efa2f870ac9f5eaed4f70
Renato Rego <renato.rego@liferay.com>
2020-07-02 09:24:29 -0300

    LPS-115095 Make sure unique field names will be always generated.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
@@ -320,12 +320,14 @@ class LayoutProvider extends Component {
 	_fieldNameGeneratorValueFn() {
 		return (desiredName, currentName, blacklist = []) => {
 			const {pages} = this.state;
+			const {generateFieldNameUsingFieldLabel} = this.props;
 
 			return generateFieldName(
 				pages,
 				desiredName,
 				currentName,
-				blacklist
+				blacklist,
+				generateFieldNameUsingFieldLabel
 			);
 		};
 	}
