6bb7970686df7a9545932c8fed24f48ee8aa817f
Marcos <marcos.martins@liferay.com>
2020-07-06 13:12:45 -0300

    LPS-115939 create a component for sidebar content

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/sidebar/Sidebar.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-report-web/src/main/resources/META-INF/resources/js/components/sidebar/Sidebar.es.js
@@ -15,98 +15,98 @@
 import ClayButton from '@clayui/button';
 import ClayIcon from '@clayui/icon';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 
 import useRequest from '../../hooks/useRequest.es';
 import List from '../list/List.es';
 import {SidebarContext} from './SidebarContext.es';
 
-export default () => {
+const SidebarContent = () => {
 	const {
 		field,
 		formReportRecordsFieldValuesURL,
-		isOpen,
 		portletNamespace,
 		toggleSidebar,
 		totalEntries,
 		type,
 	} = useContext(SidebarContext);
 
-	let endpoint = null;
+	const {origin} = new URL(formReportRecordsFieldValuesURL);
+	const path = formReportRecordsFieldValuesURL.replace(origin, '');
 
-	if (field) {
-		const {origin} = new URL(formReportRecordsFieldValuesURL);
-		const path = formReportRecordsFieldValuesURL.replace(origin, '');
-
-		endpoint = `${path}&${portletNamespace}fieldName=${field.name}`;
-	}
-	else {
-		return null;
-	}
+	const endpoint = `${path}&${portletNamespace}fieldName=${field.name}`;
 
 	const {isLoading, response: data = []} = useRequest(endpoint);
-
-	if (!isOpen) {
-		return null;
-	}
 
 	const {icon, label} = field;
 
 	return (
 		<>
-			<div className="sidebar-backdrop"></div>
-			<div className="open sidebar-container sidebar-reports">
-				<div className="sidebar sidebar-light">
-					<nav className="component-tbar tbar">
-						<div className="container-fluid">
-							<ul className="tbar-nav">
-								<li className="tbar-item">
-									<div className="icon">
-										<ClayIcon symbol={icon} />
-									</div>
-								</li>
-
-								<li className="tbar-item tbar-item-expand">
-									<div className="tbar-section">
-										<div className="field-info">
-											<p className="title">{label}</p>
-
-											<p className="description">
-												{totalEntries}{' '}
-												{Liferay.Language.get(
-													'entries'
-												).toLowerCase()}
-											</p>
-										</div>
-									</div>
-								</li>
-
-								<li className="tbar-item">
-									<ClayButton
-										className="close-sidebar"
-										displayType="secondary"
-										monospaced
-										onClick={() => toggleSidebar()}
-									>
-										<ClayIcon
-											className="close-button"
-											symbol={'times-small'}
-										/>
-									</ClayButton>
-								</li>
-							</ul>
-						</div>
-					</nav>
-
-					<div className="sidebar-body">
-						{isLoading && (
-							<div className="align-items-center d-flex loading-wrapper">
-								<ClayLoadingIndicator />
+			<nav className="component-tbar tbar">
+				<div className="container-fluid">
+					<ul className="tbar-nav">
+						<li className="tbar-item">
+							<div className="icon">
+								<ClayIcon symbol={icon} />
 							</div>
-						)}
+						</li>
 
-						<List data={data} type={type}></List>
+						<li className="tbar-item tbar-item-expand">
+							<div className="tbar-section">
+								<div className="field-info">
+									<p className="title">{label}</p>
+
+									<p className="description">
+										{totalEntries}{' '}
+										{Liferay.Language.get(
+											'entries'
+										).toLowerCase()}
+									</p>
+								</div>
+							</div>
+						</li>
+
+						<li className="tbar-item">
+							<ClayButton
+								className="close-sidebar"
+								displayType="secondary"
+								monospaced
+								onClick={() => toggleSidebar()}
+							>
+								<ClayIcon
+									className="close-button"
+									symbol={'times-small'}
+								/>
+							</ClayButton>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+			<div className="sidebar-body">
+				{isLoading && (
+					<div className="align-items-center d-flex loading-wrapper">
+						<ClayLoadingIndicator />
 					</div>
+				)}
+
+				<List data={data} type={type}></List>
+			</div>
+		</>
+	);
+};
+
+export default () => {
+	const {field, isOpen} = useContext(SidebarContext);
+
+	const ref = useRef();
+
+	return (
+		<>
+			{isOpen && <div className="sidebar-backdrop"></div>}
+			<div className="open sidebar-container sidebar-reports" ref={ref}>
+				<div className="sidebar sidebar-light">
+					{field && <SidebarContent />}
 				</div>
 			</div>
 		</>
