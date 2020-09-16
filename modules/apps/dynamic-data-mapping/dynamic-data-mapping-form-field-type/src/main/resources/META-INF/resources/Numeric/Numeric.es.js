5ae78edad191f106b8f6f9bfa6f3bc4bda042ad5
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-09-16 15:15:11 -0300

    LPS-120729 Handle cases when symbols does not correspond to the editing language symbols

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Numeric/Numeric.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Numeric/Numeric.es.js
@@ -82,9 +82,15 @@ const Numeric = ({
 		if (inputRef.current) {
 			let newValue = value;
 
+			let decimalSymbol = symbols.decimalSymbol;
+
+			if (!newValue.includes('.') && symbols.decimalSymbol != ',') {
+				decimalSymbol = ',';
+			}
+
 			if (dataType === 'integer' && value) {
 				newValue = String(
-					Math.round(newValue.replace(symbols.decimalSymbol, '.'))
+					Math.round(newValue.replace(decimalSymbol, '.'))
 				);
 			}
 
