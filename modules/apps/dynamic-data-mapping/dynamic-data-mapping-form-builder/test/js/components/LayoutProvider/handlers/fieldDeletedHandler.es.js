fb50e2fcf9401db8b4499677d30c5883264868bd
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-09-28 20:14:23 -0300

    LPS-121343 Enable tests of deleteField and remove useless test

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/test/js/components/LayoutProvider/handlers/fieldDeletedHandler.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/test/js/components/LayoutProvider/handlers/fieldDeletedHandler.es.js
@@ -15,69 +15,30 @@
 import {FormSupport} from 'dynamic-data-mapping-form-renderer';
 
 import * as fieldDeletedHandler from '../../../../../src/main/resources/META-INF/resources/js/components/LayoutProvider/handlers/fieldDeletedHandler.es';
-import RulesSupport from '../../../../../src/main/resources/META-INF/resources/js/components/RuleBuilder/RulesSupport.es';
 import mockPages from '../../../__mock__/mockPages.es';
 
 describe('LayoutProvider/handlers/fieldDeletedHandler', () => {
 	describe('handleFieldDeleted(state, event)', () => {
-		it('calls removeRow() when row is left with no fields after delete operation', () => {
+		it('calls removeEmptyRows() when row is left with no fields after delete operation', () => {
 			const event = {
-				columnIndex: 0,
-				pageIndex: 0,
-				rowIndex: 0,
+				activePage: 0,
+				fieldName: 'radio',
+				removeEmptyRows: true,
 			};
 			const state = {
 				pages: mockPages,
 				rules: [],
 			};
 
-			const removeRowSpy = jest.spyOn(FormSupport, 'removeRow');
+			const removeRowSpy = jest.spyOn(FormSupport, 'removeEmptyRows');
 
 			removeRowSpy.mockImplementation(() => []);
 
-			fieldDeletedHandler.handleFieldDeleted(state, event);
+			fieldDeletedHandler.handleFieldDeleted({}, state, event);
 
 			expect(removeRowSpy).toHaveBeenCalled();
 
 			removeRowSpy.mockRestore();
-		});
-
-		it('calls clearAllConditionFieldValues() when deleting a field used as the first operand of a condition', () => {
-			const event = {
-				columnIndex: 0,
-				pageIndex: 0,
-				rowIndex: 0,
-			};
-			const state = {
-				pages: mockPages,
-				rules: [
-					{
-						actions: [],
-						conditions: [
-							{
-								operands: [
-									{
-										value: 'radio',
-									},
-								],
-							},
-						],
-					},
-				],
-			};
-
-			const clearAllConditionFieldValuesSpy = jest.spyOn(
-				RulesSupport,
-				'clearAllConditionFieldValues'
-			);
-
-			clearAllConditionFieldValuesSpy.mockImplementation(() => []);
-
-			fieldDeletedHandler.handleFieldDeleted(state, event);
-
-			expect(clearAllConditionFieldValuesSpy).toHaveBeenCalled();
-
-			clearAllConditionFieldValuesSpy.mockRestore();
 		});
 	});
 });
