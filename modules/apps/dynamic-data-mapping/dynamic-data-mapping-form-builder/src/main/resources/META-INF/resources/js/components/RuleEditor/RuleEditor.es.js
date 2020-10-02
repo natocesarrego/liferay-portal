df6e2d929e4011e5b8dd33bae52c3a7b7db520a5
Marcos <marcos.martins@liferay.com>
2020-10-02 15:21:19 -0300

    LPS-120149 pass rows and columns attributes to soy template

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
@@ -314,7 +314,9 @@ class RuleEditor extends Component {
 
 		const conditions = state.conditions.map((condition) => {
 			const fieldName = condition.operands[0].value;
+			let firstOperandColumns = [];
 			let firstOperandOptions = [];
+			let firstOperandRows = [];
 			let operators = [];
 
 			if (fieldName) {
@@ -322,18 +324,36 @@ class RuleEditor extends Component {
 
 				operators = this._getOperatorsByFieldType(dataType);
 
-				firstOperandOptions = RulesSupport.getFieldOptions(
-					fieldName,
-					pages
-				);
+				const {columns, rows} = this.getFieldColumnsAndRows(fieldName);
+
+				firstOperandColumns = columns;
+				firstOperandRows = rows;
+
+				firstOperandOptions = this.getFieldOptions(fieldName);
 			}
 
 			return {
 				...condition,
 				binaryOperator: this._isBinary(condition.operator),
+				firstOperandColumns,
 				firstOperandOptions,
+				firstOperandRows,
 				operands: condition.operands.map((operand, index) => {
 					if (index === 1 && this.isValueOperand(operand)) {
+						const firstOperandType = getFieldProperty(
+							pages,
+							condition.operands[0].value,
+							'type'
+						);
+
+						if (
+							firstOperandType === 'grid' &&
+							!!operand.value &&
+							typeof operand.value == 'string'
+						) {
+							operand.value = JSON.parse(operand.value);
+						}
+
 						operand = {
 							...operand,
 							dataType: getFieldProperty(
@@ -341,11 +361,7 @@ class RuleEditor extends Component {
 								condition.operands[0].value,
 								'dataType'
 							),
-							type: getFieldProperty(
-								pages,
-								condition.operands[0].value,
-								'type'
-							),
+							type: firstOperandType,
 						};
 					}
 
