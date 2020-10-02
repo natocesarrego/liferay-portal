b63a8f4ab462e4fd240cb18c09ccac1771e3d78a
Marcos <marcos.martins@liferay.com>
2020-10-02 15:23:20 -0300

    LPS-120149 operand value can be a json

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
@@ -1750,7 +1750,7 @@ RuleEditor.STATE = {
 					label: Config.string(),
 					repeatable: Config.bool(),
 					type: Config.string(),
-					value: Config.string(),
+					value: Config.any(),
 				})
 			),
 			operator: Config.string(),
