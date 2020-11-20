763d9446d938194f68663f77063afa918537502d
Carolina <carolina.barbosa@liferay.com>
2020-11-20 11:55:01 -0300

    LPS-123543 Validate file size

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMFormUploadFileEntryHandler.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/upload/DDMFormUploadFileEntryHandler.java
@@ -64,6 +64,8 @@ public class DDMFormUploadFileEntryHandler implements UploadFileEntryHandler {
 
 			file = FileUtil.createTempFile(inputStream);
 
+			_ddmFormUploadValidator.validateFileSize(file, fileName);
+
 			ThemeDisplay themeDisplay =
 				(ThemeDisplay)uploadPortletRequest.getAttribute(
 					WebKeys.THEME_DISPLAY);
@@ -112,6 +114,9 @@ public class DDMFormUploadFileEntryHandler implements UploadFileEntryHandler {
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private DDMFormUploadValidator _ddmFormUploadValidator;
 
 	@Reference
 	private UserLocalService _userLocalService;
