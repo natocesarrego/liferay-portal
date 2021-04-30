9116bbcc17885a6d77f3ec4904fc9e2eb442503d
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-04-30 16:41:12 -0300

    LPS-130739 Change tracker parameter to string

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/form/page/change/DDMFormPageChangeTrackerImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/form/page/change/DDMFormPageChangeTrackerImpl.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Deactivate;
 public class DDMFormPageChangeTrackerImpl implements DDMFormPageChangeTracker {
 
 	@Override
-	public DDMFormPageChange getDDMFormPageChangeById(long id) {
+	public DDMFormPageChange getDDMFormPageChangeById(String id) {
 		return _ddmFormPageChangeIdTrackerMap.getService(id);
 	}
 
@@ -48,7 +48,7 @@ public class DDMFormPageChangeTrackerImpl implements DDMFormPageChangeTracker {
 		_ddmFormPageChangeIdTrackerMap.close();
 	}
 
-	private ServiceTrackerMap<Long, DDMFormPageChange>
+	private ServiceTrackerMap<String, DDMFormPageChange>
 		_ddmFormPageChangeIdTrackerMap;
 
 }
