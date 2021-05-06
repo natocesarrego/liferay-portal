6ea39b84b7832d61961e14f2165171ad79e53425
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-06 10:25:19 -0300

    LPS-131505 sf

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/test/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/helper/DDMFormEvaluatorHelperTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-evaluator-impl/src/test/java/com/liferay/dynamic/data/mapping/form/evaluator/internal/helper/DDMFormEvaluatorHelperTest.java
@@ -1638,7 +1638,7 @@ public class DDMFormEvaluatorHelperTest extends PowerMockito {
 
 		DDMFormEvaluatorHelper ddmFormEvaluatorHelper =
 			new DDMFormEvaluatorHelper(
-				builder.build(), _ddmExpressionFactory,
+				_ddmExpressionFactory, builder.build(),
 				mockDDMFormFieldTypeServicesTracker(),
 				mockDDMFormPageChangeTracker());
 
