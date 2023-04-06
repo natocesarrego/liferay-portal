50a21d210868e5e209ebc528df286149003f2bf1
Nícolas Moura <nicolas_moura02@outlook.com>
2023-04-06 16:27:48 -0300

    LPS-181075 Create VelocityMigrationUtil to be used in all Velocity Migration Checks

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/util/VelocityMigrationUtil.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check.util;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.source.formatter.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Nícolas Moura
+ */
+public class VelocityMigrationUtil {
+
+	public static String getFTLContentMigrated(
+			String content, String velocityFileName)
+		throws IOException {
+
+		File file = _getMigratedFTLFile(velocityFileName);
+
+		String contentFTL = content;
+
+		if (file.length() != 0) {
+			contentFTL = FileUtil.read(file);
+		}
+
+		return contentFTL;
+	}
+
+	public static void writeMigratedContent(
+			String migratedContent, String velocityFileName)
+		throws IOException {
+
+		FileUtil.write(_getMigratedFTLFile(velocityFileName), migratedContent);
+	}
+
+	private static File _getMigratedFTLFile(String fileName) {
+		int periodIndex = fileName.lastIndexOf(StringPool.PERIOD);
+		int slashIndex = fileName.lastIndexOf(StringPool.SLASH);
+
+		return new File(
+			StringBundler.concat(
+				fileName.substring(0, slashIndex), "/migrated/",
+				fileName.substring(slashIndex + 1, periodIndex), ".ftl"));
+	}
+
+}
