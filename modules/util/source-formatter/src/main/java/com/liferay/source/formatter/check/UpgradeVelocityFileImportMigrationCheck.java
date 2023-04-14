e02bf7a734117a9a5ca5be50188e356b645b83bc
Nícolas Moura <nicolas_moura02@outlook.com>
2023-04-14 19:07:27 -0300

    LPS-181820 Create UpgradeVelocityFileImportMigrationCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeVelocityFileImportMigrationCheck.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.VelocityMigrationUtil;
+
+import java.io.IOException;
+
+/**
+ * @author Nícolas Moura
+ */
+public class UpgradeVelocityFileImportMigrationCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws IOException {
+
+		if (!fileName.endsWith(".vm")) {
+			return content;
+		}
+
+		VelocityMigrationUtil.writeMigratedContent(
+			_formatContent(
+				VelocityMigrationUtil.getFreeMarkerMigratedContent(
+					content, fileName)),
+			fileName);
+
+		return content;
+	}
+
+	private String _formatContent(String content) {
+		String[] lines = content.split(StringPool.NEW_LINE);
+
+		for (String line : lines) {
+			if (line.contains("#parse")) {
+				String newImport = line.replace("#parse", "<#include");
+
+				newImport = StringUtil.removeFirst(
+					newImport, StringPool.OPEN_PARENTHESIS);
+				newImport = StringUtil.replaceLast(
+					newImport, CharPool.CLOSE_PARENTHESIS, " />");
+
+				content = StringUtil.replace(content, line, newImport);
+			}
+		}
+
+		return content;
+	}
+
+}
