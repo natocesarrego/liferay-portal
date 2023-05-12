a812891d49bd61b8e6feea957cc51c83de0a86b5
Nícolas Moura <nicolas_moura02@outlook.com>
2023-05-12 16:13:38 -0300

    LPS-184043 Change UpgradeJavaCheck implementation to consume the imports.txt file from inside of SF folder

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeJavaCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeJavaCheck.java
@@ -65,7 +65,7 @@ public class UpgradeJavaCheck extends BaseFileCheck {
 
 	private synchronized Map<String, String> _getImportsMap() throws Exception {
 		if (_importsMap == null) {
-			_importsMap = _getMap("/java/imports.txt");
+			_importsMap = _getMap("/imports.txt");
 		}
 
 		return _importsMap;
@@ -76,7 +76,8 @@ public class UpgradeJavaCheck extends BaseFileCheck {
 
 		File importsFile = SourceFormatterUtil.getFile(
 			getBaseDirName(),
-			SourceFormatterUtil.UPGRADE_INPUT_DATA_DIRECTORY_NAME + fileName,
+			"modules/util/source-formatter/src/main/resources/dependencies/" +
+				fileName,
 			getMaxDirLevel());
 
 		if (importsFile == null) {
@@ -85,17 +86,12 @@ public class UpgradeJavaCheck extends BaseFileCheck {
 
 		String[] lines = StringUtil.splitLines(FileUtil.read(importsFile));
 
-		String oldValue = null;
-
 		for (String line : lines) {
-			if (line.matches("\\d+\\.old:.+")) {
-				oldValue = line.substring(line.indexOf(":") + 1);
-			}
-			else if (line.matches("\\d+\\.new:.+") && (oldValue != null)) {
-				map.put(oldValue, line.substring(line.indexOf(":") + 1));
+			int separatorIndex = line.indexOf("=");
 
-				oldValue = null;
-			}
+			map.put(
+				line.substring(0, separatorIndex),
+				line.substring(separatorIndex + 1));
 		}
 
 		return map;
