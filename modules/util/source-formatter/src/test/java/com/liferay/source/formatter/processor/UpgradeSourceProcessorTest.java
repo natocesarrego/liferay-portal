d7a84393e9f19f617d013db6e366e767b3df6162
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-06-02 18:15:16 -0300

    LPS-185518 Create test UpgradeJavaSetResultsAndTotalCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -69,6 +69,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeJavaSetResultsAndTotalCheck() throws Exception {
+		test("upgrade/UpgradeJavaSetResultsAndTotalCheck.testjava");
+	}
+
+	@Test
 	public void testUpgradeVelocityMigrationCheck() throws Exception {
 		test(
 			SourceProcessorTestParameters.create(
