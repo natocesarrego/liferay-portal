b468272f82b095a98fc76f9760884a316d119e5a
Tamyris Torres <tamyris.bernardo@liferay.com>
2023-06-07 14:55:11 -0300

    LPS-186809 Create Test UpgradeJavaGetPortletGroupIdMethodCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -69,6 +69,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeJavaGetPortletGroupIdMethodCheck() throws Exception {
+		test("upgrade/UpgradeJavaGetPortletGroupIdMethodCheck.testjava");
+	}
+
+	@Test
 	public void testUpgradeJavaServiceTrackerListCheck() throws Exception {
 		test("upgrade/UpgradeJavaServiceTrackerListCheck.testjava");
 	}
