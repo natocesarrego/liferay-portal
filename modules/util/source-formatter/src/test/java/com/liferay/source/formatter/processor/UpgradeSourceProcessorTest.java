278679140a94eb7a3ee5349dc15256528a126d13
Michael Cavalcanti <michael.cavalcanti@liferay.com>
2023-06-29 15:12:07 -0300

    LPS-189225 Create test for UpgradeJavaGetLayoutDisplayPageProviderCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -81,6 +81,13 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeJavaGetLayoutDisplayPageProviderCheck()
+		throws Exception {
+
+		test("upgrade/UpgradeJavaGetLayoutDisplayPageProviderCheck.testjava");
+	}
+
+	@Test
 	public void testUpgradeJavaModelPermissionsCheck() throws Exception {
 		test("upgrade/UpgradeJavaModelPermissionsCheck.testjava");
 	}
