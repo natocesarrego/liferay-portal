59991f6873dba457b27f4d634aaf85efa3554d7e
Albert Gomes <albert.gomes@liferay.com>
2023-07-12 10:38:56 -0300

    LPS-187428 Create upgrade process to remove declared browser sniffer variables

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v5_3_2/TemplateBrowserSnifferUpgradeProcess.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.dynamic.data.mapping.internal.upgrade.v5_3_2;
+
+import com.liferay.dynamic.data.mapping.internal.upgrade.BaseTemplateUpgradeProcess;
+import com.liferay.petra.string.StringPool;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class TemplateBrowserSnifferUpgradeProcess
+	extends BaseTemplateUpgradeProcess {
+
+	@Override
+	protected Pattern getTemplatePattern() throws Exception {
+		return Pattern.compile(
+			"\\w*\\s*\\=\\s*.+com\\.liferay\\.portal\\.kernel\\.servlet\\." +
+				"BrowserSnifferUtil\\\"\\)");
+	}
+
+	@Override
+	protected String getTemplatePatternReplacement() throws Exception {
+		return StringPool.BLANK;
+	}
+
+}
