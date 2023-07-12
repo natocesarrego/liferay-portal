df81c116cac6be4a977403a73ad8459994a97e77
Nícolas Moura <nicolas_moura02@outlook.com>
2023-07-12 14:19:36 -0300

    LPS-189915 Create test for UpgradeJavaCommerceCountryServiceCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -85,6 +85,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testUpgradeJavaCommerceCountryServiceCheck() throws Exception {
+		test("upgrade/UpgradeJavaCommerceCountryServiceCheck.testjava");
+	}
+
+	@Test
 	public void testUpgradeJavaCommerceRegionCheck() throws Exception {
 		test("upgrade/UpgradeJavaCommerceRegionCheck.testjava");
 	}
