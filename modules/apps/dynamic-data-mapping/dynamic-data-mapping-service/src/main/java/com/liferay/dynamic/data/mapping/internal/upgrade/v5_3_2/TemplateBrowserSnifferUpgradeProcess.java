f7323fc8618d6371bb93d852f3e7cd9d66a22eac
Felipe Veloso <felipe.veloso@liferay.com>
2023-07-21 11:14:55 -0300

    LPS-187428 Update TemplateBrowserSnifferUpgradeProcess Class

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v5_3_2/TemplateBrowserSnifferUpgradeProcess.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v5_3_2/TemplateBrowserSnifferUpgradeProcess.java
@@ -17,9 +17,14 @@ public class TemplateBrowserSnifferUpgradeProcess
 	extends BaseTemplateUpgradeProcess {
 
 	@Override
+	protected String getTemplateContextVariable() {
+		return "browserSniffer";
+	}
+
+	@Override
 	protected Pattern getTemplatePattern() throws Exception {
 		return Pattern.compile(
-			"\\w*\\s*\\=\\s*.+com\\.liferay\\.portal\\.kernel\\.servlet\\." +
+			"\\w+\\s*\\=\\s*.+com\\.liferay\\.portal\\.kernel\\.servlet\\." +
 				"BrowserSnifferUtil\\\"\\)");
 	}
 
