4c2d9d1e328ada6f87a69bd434cc1b286ed901b4
Nícolas Moura <nicolas_moura02@outlook.com>
2023-09-21 18:56:46 -0300

    LPS-192413 Remove code replaced by UpgradeCatchAllCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -161,11 +161,6 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
-	public void testUpgradeJavaCommerceRegionCheck() throws Exception {
-		test("upgrade/UpgradeJavaCommerceRegionCheck.testjava");
-	}
-
-	@Test
 	public void testUpgradeJavaCommerceShippingOptionCheck() throws Exception {
 		test("upgrade/UpgradeJavaCommerceShippingOptionCheck.testjava");
 	}
@@ -189,11 +184,6 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 				"DLFolderLocalService, DLFolderServiceUtil and ",
 				"DLFolderLocalServiceUtil. Fill the new parameter manually, ",
 				"see LPS-194001."));
-	}
-
-	@Test
-	public void testUpgradeJavaExtractTextMethodCheck() throws Exception {
-		test("upgrade/UpgradeJavaExtractTextMethodCheck.testjava");
 	}
 
 	@Test
@@ -306,18 +296,6 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 		throws Exception {
 
 		test("upgrade/UpgradeJavaSchedulerEntryImplConstructorCheck.testjava");
-	}
-
-	@Test
-	public void testUpgradeJavaSearchVocabulariesMethodCheck()
-		throws Exception {
-
-		test(
-			"upgrade/UpgradeJavaSearchVocabulariesMethodCheck.testjava",
-			StringBundler.concat(
-				"Unable to format searchVocabularies method from ",
-				"AssetVocabularyService and AssetVocabularyLocalService. Fill ",
-				"the new parameters manually, see LPS-189866"));
 	}
 
 	@Test
