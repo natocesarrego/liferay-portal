0ecc3b73e2d5cfc4452ff310cab4320970b45ab8
Davi Santos <davi.santos@liferay.com>
2025-06-20 10:30:54 -0300

    LPD-58579 Add PlaywrightTest class to represent test metadata

---
+++ tools/test-report-generator/src/main/java/com/liferay/report/record/csv/playwright/PlaywrightTest.java
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.report.record.csv.playwright;
+
+/**
+ * @author Davi Santos
+ */
+public class PlaywrightTest {
+
+	public PlaywrightTest(
+		String className, String testFilePath, String testName,
+		boolean ignored) {
+
+		_className = className;
+		_testFilePath = testFilePath;
+		_testName = testName;
+		_ignored = ignored;
+	}
+
+	public String getClassName() {
+		return _className;
+	}
+
+	public String getTestFilePath() {
+		return _testFilePath;
+	}
+
+	public String getTestName() {
+		return _testName;
+	}
+
+	public boolean isIgnored() {
+		return _ignored;
+	}
+
+	private final String _className;
+	private final boolean _ignored;
+	private final String _testFilePath;
+	private final String _testName;
+
+}
