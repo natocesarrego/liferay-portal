795f68521b04f7751b4585e7a11579d3057fecd9
Davi Santos <davi.santos@liferay.com>
2025-06-20 10:31:58 -0300

    LPD-58579 Add PlaywrightTestSelector to collect all Playwright tests from playwright/ directory

---
+++ tools/test-report-generator/src/main/java/com/liferay/report/record/csv/playwright/PlaywrightTestSelector.java
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.report.record.csv.playwright;
+
+import java.io.IOException;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author Davi Santos
+ */
+public class PlaywrightTestSelector {
+
+	public List<PlaywrightTest> getPlaywrightTests() {
+		List<PlaywrightTest> playwrightTests = new ArrayList<>();
+
+		String projectRoot = System.getProperty("user.dir");
+
+		String playwrightTestsDir =
+			projectRoot + "/../../modules/test/playwright/tests";
+
+		try {
+			Files.walkFileTree(
+				Paths.get(playwrightTestsDir),
+				new SimpleFileVisitor<>() {
+
+					@Override
+					public FileVisitResult visitFile(
+						Path file, BasicFileAttributes basicFileAttributes) {
+
+						if (file.toString(
+							).endsWith(
+								".spec.ts"
+							)) {
+
+							playwrightTests.addAll(
+								PlaywrightTestParser.parse(file));
+						}
+
+						return FileVisitResult.CONTINUE;
+					}
+
+				});
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+
+		playwrightTests.sort(
+			Comparator.comparing(PlaywrightTest::getClassName));
+
+		return playwrightTests;
+	}
+
+}
