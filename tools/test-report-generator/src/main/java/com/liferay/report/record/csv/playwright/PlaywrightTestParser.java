b0c1e4d2d624bb56ed6ceba4268601353d4bb00d
Davi Santos <davi.santos@liferay.com>
2025-06-20 10:32:48 -0300

    LPD-58579 Add PlaywrightTestParser to parse test metadata from Playwright test files

---
+++ tools/test-report-generator/src/main/java/com/liferay/report/record/csv/playwright/PlaywrightTestParser.java
@@ -0,0 +1,64 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.report.record.csv.playwright;
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Davi Santos
+ */
+public class PlaywrightTestParser {
+
+	public static List<PlaywrightTest> parse(Path playwrightTestFilePath) {
+		String playwrightTestFileContent = "";
+
+		try {
+			playwrightTestFileContent = Files.readString(
+				playwrightTestFilePath);
+		}
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+
+		List<PlaywrightTest> playwrightTests = new ArrayList<>();
+
+		Matcher matcher = _testPattern.matcher(playwrightTestFileContent);
+
+		String className = playwrightTestFilePath.getFileName(
+		).toString(
+		).replaceFirst(
+			"\\.spec\\.ts$", ""
+		);
+		String testFilePath = playwrightTestFilePath.toString();
+
+		while (matcher.find()) {
+			String testName = matcher.group(2);
+
+			boolean ignored = false;
+
+			if (matcher.group(1) != null) {
+				ignored = true;
+			}
+
+			playwrightTests.add(
+				new PlaywrightTest(className, testFilePath, testName, ignored));
+		}
+
+		return playwrightTests;
+	}
+
+	private static final Pattern _testPattern = Pattern.compile(
+		"test(?:\\.skip)?\\(\\s*(['\"`])(.*?)\\1");
+
+}
