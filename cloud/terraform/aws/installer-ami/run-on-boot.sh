cfa69bf1888a4bede8fa89a3e4291ceb7872042d
Raymond Augé <raymond.auge@liferay.com>
2025-10-14 15:11:55 -0400

    LPD-63872 manual SF shell scripts

--- cloud/terraform/aws/installer-ami/run-on-boot.sh
+++ cloud/terraform/aws/installer-ami/run-on-boot.sh
@@ -1,79 +1,148 @@
 #!/bin/bash
 
-echo "Executing run-on-boot.sh at $(date) by $(id)"
+set -eux
 
-TOKEN=$(curl --header "X-aws-ec2-metadata-token-ttl-seconds: 21600" --request PUT "http://169.254.169.254/latest/api/token")
+function main {
+    echo "Executing run-on-boot.sh at $(date) by $(id)"
 
-AMI_ID=$(curl --header "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/ami-id)
-INSTANCE_ID=$(curl --header "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(curl --header "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/placement/region)
+    local token
 
-echo "AMI_ID=${AMI_ID}"
-echo "INSTANCE_ID=${INSTANCE_ID}"
-echo "REGION=${REGION}"
-echo "CALLER_IDENTITY=$(aws sts get-caller-identity)"
+    token=$( \
+        curl \
+        --header "X-aws-ec2-metadata-token-ttl-seconds: 21600" \
+        --request PUT \
+        "http://169.254.169.254/latest/api/token")
 
-tree -a /opt/liferay
+    local ami_id
 
-CHART_DIR=/opt/liferay/chart
-IMAGE_DIR=/opt/liferay/image
-TERRAFORM_DIR=/opt/liferay/terraform
+    ami_id=$( \
+        curl \
+            --header "X-aws-ec2-metadata-token: ${token}" \
+            http://169.254.169.254/latest/meta-data/ami-id)
 
-# ERC
-pushd "${TERRAFORM_DIR}/ecr"
+    local instance_id
 
-terraform apply -auto-approve \
-	-var="deployment_name=lfr-ami" \
-	-var="region=${REGION}"
-terraform output > "${TERRAFORM_DIR}/eks/terraform.tfvars"
+    instance_id=$( \
+        curl \
+            --header "X-aws-ec2-metadata-token: ${token}" \
+            http://169.254.169.254/latest/meta-data/instance-id)
 
-# DXP Image
-DXP_IMAGE_TAG=$(oras repo tags --oci-layout "${IMAGE_DIR}/dxp")
-ECR_DXP_REPOSITORY_URL=$(terraform output -json ecr_repositories | jq --raw-output '."liferay/dxp".url')
-ECR_REGISTRY_URL=${ECR_DXP_REPOSITORY_URL%%/*}
+    local region
 
-aws ecr get-login-password --region "${REGION}" | \
-	oras login --username AWS --password-stdin "${ECR_REGISTRY_URL}"
+    region=$( \
+        curl \
+            --header "X-aws-ec2-metadata-token: ${token}" \
+            http://169.254.169.254/latest/meta-data/placement/region)
 
-oras cp --from-oci-layout --no-tty "${IMAGE_DIR}/dxp:${DXP_IMAGE_TAG}" "${ECR_DXP_REPOSITORY_URL}:${DXP_IMAGE_TAG}"
+    echo "The region is ${region}"
 
-popd
+    echo "The caller identity is $(aws sts get-caller-identity)"
 
-# EKS
-pushd "${TERRAFORM_DIR}/eks"
+    tree -a /opt/liferay
 
-terraform apply -auto-approve -var node_instance_type=t3.2xlarge
-terraform output > "${TERRAFORM_DIR}/dependencies/terraform.tfvars"
+    local chart_dir
 
-aws eks update-kubeconfig \
-	--name $(terraform output -raw cluster_name) \
-	--region $(terraform output -raw region)
+    chart_dir=/opt/liferay/chart
 
-kubectl cluster-info
+    local image_dir
 
-popd
+    image_dir=/opt/liferay/image
 
-# OpenSearch/Postgres/S3
-pushd "${TERRAFORM_DIR}/dependencies"
+    local terraform_dir
 
-terraform apply -auto-approve
+    terraform_dir=/opt/liferay/terraform
 
-# Helm Install
-if [ -f /opt/liferay/values.yaml ]
-then
-	VALUES_FILE_ARG="--values /opt/liferay/values.yaml"
-else
-	VALUES_FILE_ARG=""
-fi
+    pushd "${terraform_dir}/ecr"
 
-helm upgrade liferay "${CHART_DIR}/liferay-aws" \
-	--install \
-	--namespace "$(terraform output -raw deployment_namespace)" \
-	--set "liferay-default.image.repository=${ECR_DXP_REPOSITORY_URL}" \
-	--set "liferay-default.image.tag=${DXP_IMAGE_TAG}" \
-	--set "liferay-default.serviceAccount.annotations.eks\.amazonaws\.com/role-arn=$(terraform output -raw liferay_sa_role)" \
-	${VALUES_FILE_ARG}
+    terraform apply -auto-approve \
+        -var="deployment_name=lfr-ami" \
+        -var="region=${region}"
+    terraform output > "${terraform_dir}/eks/terraform.tfvars"
 
-kubectl rollout status statefulset/liferay-default \
-	--namespace "$(terraform output -raw deployment_namespace)" \
-	--timeout=1200s
+    local dxp_image_tag
+
+    dxp_image_tag=$(oras repo tags --oci-layout "${image_dir}/dxp")
+
+    local ecr_dxp_repository_url
+
+    ecr_dxp_repository_url=$( \
+        terraform \
+            output \
+            -json ecr_repositories \
+            | jq --raw-output '."liferay/dxp".url')
+
+    local ecr_registry_url
+
+    ecr_registry_url=${ecr_dxp_repository_url%%/*}
+
+    aws \
+        ecr \
+        get-login-password \
+        --region "${region}" \
+        | oras login --username AWS --password-stdin "${ecr_registry_url}"
+
+    oras \
+        cp \
+        --from-oci-layout \
+        --no-tty \
+        "${image_dir}/dxp:${dxp_image_tag}" \
+        "${ecr_dxp_repository_url}:${dxp_image_tag}"
+
+    popd
+
+    pushd "${terraform_dir}/eks"
+
+    terraform apply -auto-approve -var node_instance_type=t3.2xlarge
+    terraform output > "${terraform_dir}/dependencies/terraform.tfvars"
+
+    aws \
+        eks \
+        update-kubeconfig \
+        --name $(terraform output -raw cluster_name) \
+        --region $(terraform output -raw region)
+
+    kubectl cluster-info
+
+    popd
+
+    pushd "${terraform_dir}/dependencies"
+
+    terraform apply -auto-approve
+
+    local values_file_arg
+
+    if [ -f /opt/liferay/values.yaml ]
+    then
+        values_file_arg="--values /opt/liferay/values.yaml"
+    else
+        values_file_arg=""
+    fi
+
+    local namespace
+
+    namespace=$(terraform output -raw deployment_namespace)
+
+    local liferay_sa_role
+
+    liferay_sa_role=$(terraform output -raw liferay_sa_role)
+
+    helm \
+        upgrade \
+        liferay \
+        "${chart_dir}/liferay-aws" \
+        --install \
+        --namespace "${namespace}" \
+        --set "liferay-default.image.repository=${ecr_dxp_repository_url}" \
+        --set "liferay-default.image.tag=${dxp_image_tag}" \
+        --set "liferay-default.serviceAccount.annotations.eks\.amazonaws\.com/role-arn=${liferay_sa_role}" \
+        ${values_file_arg}
+
+    kubectl \
+        rollout \
+        status \
+        statefulset/liferay-default \
+        --namespace "${namespace}" \
+        --timeout=1200s
+}
+
+main
