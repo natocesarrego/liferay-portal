a2a6882e13dde2da0b3ccd9521b598bf37a647c7
Raymond Augé <raymond.auge@liferay.com>
2025-10-17 14:24:57 -0400

    LPD-63872 source formatting

--- cloud/terraform/aws/installer-ami/run_on_boot.sh
+++ cloud/terraform/aws/installer-ami/run_on_boot.sh
@@ -5,31 +5,13 @@ set -eux
 function main {
 	echo "Executing run_on_boot.sh at $(date) by $(id)"
 
-	local token
-
-	token=$( \
+	local token=$( \
 		curl \
 		--header "X-aws-ec2-metadata-token-ttl-seconds: 21600" \
 		--request PUT \
 		"http://169.254.169.254/latest/api/token")
 
-	local ami_id
-
-	ami_id=$( \
-		curl \
-			--header "X-aws-ec2-metadata-token: ${token}" \
-			http://169.254.169.254/latest/meta-data/ami-id)
-
-	local instance_id
-
-	instance_id=$( \
-		curl \
-			--header "X-aws-ec2-metadata-token: ${token}" \
-			http://169.254.169.254/latest/meta-data/instance-id)
-
-	local region
-
-	region=$( \
+	local region=$( \
 		curl \
 			--header "X-aws-ec2-metadata-token: ${token}" \
 			http://169.254.169.254/latest/meta-data/placement/region)
@@ -40,46 +22,33 @@ function main {
 
 	tree -a /opt/liferay
 
-	local chart_dir
+	local image_dir=/opt/liferay/image
 
-	chart_dir=/opt/liferay/chart
-
-	local image_dir
-
-	image_dir=/opt/liferay/image
-
-	local terraform_dir
-
-	terraform_dir=/opt/liferay/terraform
+	local terraform_dir=/opt/liferay/terraform
 
 	pushd "${terraform_dir}/ecr"
 
 	terraform apply -auto-approve \
 		-var="deployment_name=lfr-ami" \
 		-var="region=${region}"
+
 	terraform output > "${terraform_dir}/eks/terraform.tfvars"
 
-	local dxp_image_tag
-
-	dxp_image_tag=$(oras repo tags --oci-layout "${image_dir}/dxp")
-
-	local ecr_dxp_repository_url
-
-	ecr_dxp_repository_url=$( \
+	local ecr_dxp_repository_url=$( \
 		terraform \
 			output \
 			-json ecr_repositories \
 			| jq --raw-output '."liferay/dxp".url')
 
-	local ecr_registry_url
-
-	ecr_registry_url=${ecr_dxp_repository_url%%/*}
+	local ecr_registry_url=${ecr_dxp_repository_url%%/*}
 
 	aws \
 		ecr \
 		get-login-password \
 		--region "${region}" \
 		| oras login --username AWS --password-stdin "${ecr_registry_url}"
+
+	local dxp_image_tag=$(oras repo tags --oci-layout "${image_dir}/dxp")
 
 	oras \
 		cp \
@@ -93,6 +62,7 @@ function main {
 	pushd "${terraform_dir}/eks"
 
 	terraform apply -auto-approve -var node_instance_type=t3.2xlarge
+
 	terraform output > "${terraform_dir}/dependencies/terraform.tfvars"
 
 	aws \
@@ -118,18 +88,14 @@ function main {
 		values_file_arg=""
 	fi
 
-	local namespace
+	local namespace=$(terraform output -raw deployment_namespace)
 
-	namespace=$(terraform output -raw deployment_namespace)
-
-	local liferay_sa_role
-
-	liferay_sa_role=$(terraform output -raw liferay_sa_role)
+	local liferay_sa_role=$(terraform output -raw liferay_sa_role)
 
 	helm \
 		upgrade \
 		liferay \
-		"${chart_dir}/liferay-aws" \
+		/opt/liferay/chart/liferay-aws \
 		--install \
 		--namespace "${namespace}" \
 		--set "liferay-default.image.repository=${ecr_dxp_repository_url}" \
