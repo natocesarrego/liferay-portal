0072ca06220e8bb9c055aa801a77bd36c161751c
Brian Greenwald <brian.greenwald@liferay.com>
2026-02-27 09:28:37 -0600

    LCD-49975 SF

--- cloud/scripts/setup_aws.sh
+++ cloud/scripts/setup_aws.sh
@@ -4,26 +4,48 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPTS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-ROOT_CLOUD_DIR=$(cd "${SCRIPTS_DIR}/.." && pwd)
+_SCRIPTS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+_ROOT_CLOUD_DIR=$(cd "${_SCRIPTS_DIR}/.." && pwd)
 
-function generate_tfvars {
-	local json_file="$1"
-	local tfvars_file="$2"
-
-	if [ ! -f "$json_file" ]
+function main {
+	if [ "${#}" -ne 1 ]
 	then
-		echo "No JSON file not found at: ${json_file}"
+		echo "Usage: ${0} <path_to_config_json_file>"
 
 		exit 1
 	fi
 
-	if ! jq -e '.variables | objects' "$json_file" > /dev/null
+	_generate_tfvars "${1}" "${_SCRIPTS_DIR}/global_terraform.tfvars"
+
+	echo "Attempting to login to your AWS account via SSO..."
+
+	aws sso login
+
+	_setup_aws_eks
+
+	_setup_aws_gitops
+
+	_port_forward_argocd
+}
+
+function _generate_tfvars {
+	local json_file="${1}"
+
+	if [ ! -f "${json_file}" ]
+	then
+		echo "No JSON file ${json_file} found."
+
+		exit 1
+	fi
+
+	if ! jq --exit-status '.variables | objects' "${json_file}" > /dev/null
 	then
 		echo "The JSON file must contain a root object named 'variables'."
 
 		exit 1
 	fi
+
+	local tfvars_file="${2}"
 
 	echo "Generating ${tfvars_file} from ${json_file}..."
 
@@ -40,9 +62,9 @@ function generate_tfvars {
 				"\(.key) = \(.value | @json)"
 			else
 				"\(.key) = \(.value)"
-			end' "$json_file")
+			end' "${json_file}")
 
-	if [ -z "$tfvars_content" ]
+	if [ -z "${tfvars_content}" ]
 	then
 		echo "The 'variables' object in the JSON file is empty. You will be prompted for all required variables."
 
@@ -54,29 +76,12 @@ function generate_tfvars {
 	echo "${tfvars_file} generated successfully."
 }
 
-function main {
-	if [ "$#" -ne 1 ]
-	then
-		echo "Usage: $0 <path_to_config_json_file>"
-
-		exit 1
-	fi
-
-	generate_tfvars "$1" "${SCRIPTS_DIR}/global_terraform.tfvars"
-
-	echo "Attempting to login to your AWS account via SSO..."
-
-	aws sso login
-
-	setup_aws_eks
-
-	setup_aws_gitops
-
-	port_forward_argocd
+function _popd {
+	popd > /dev/null
 }
 
-function port_forward_argocd {
-	_pushd "${ROOT_CLOUD_DIR}/terraform/aws/gitops/platform"
+function _port_forward_argocd {
+	_pushd "${_ROOT_CLOUD_DIR}/terraform/aws/gitops/platform"
 
 	local argocd_namespace=$(terraform output -raw argocd_namespace)
 
@@ -102,12 +107,16 @@ function port_forward_argocd {
 	_popd
 }
 
-function setup_aws_eks {
-	_pushd "${ROOT_CLOUD_DIR}/terraform/aws/eks"
+function _pushd {
+	pushd "${1}" > /dev/null
+}
+
+function _setup_aws_eks {
+	_pushd "${_ROOT_CLOUD_DIR}/terraform/aws/eks"
 
 	echo "Setting up the AWS EKS cluster..."
 
-	terraform_init_and_apply "."
+	_terraform_init_and_apply "."
 
 	aws \
 		eks \
@@ -120,36 +129,28 @@ function setup_aws_eks {
 	_popd
 }
 
-function setup_aws_gitops {
-	_pushd "${ROOT_CLOUD_DIR}/terraform/aws/gitops"
+function _setup_aws_gitops {
+	_pushd "${_ROOT_CLOUD_DIR}/terraform/aws/gitops"
 
 	echo "Setting up GitOps Infrastructure..."
 
-	terraform_init_and_apply "./platform"
+	_terraform_init_and_apply "./platform"
 
-	terraform_init_and_apply "./resources"
+	_terraform_init_and_apply "./resources"
 
 	echo "GitOps Infrastructure setup complete."
 
 	_popd
 }
 
-function terraform_init_and_apply {
-	_pushd "$1"
+function _terraform_init_and_apply {
+	_pushd "${1}"
 
 	terraform init
 
-	terraform apply "-var-file=${SCRIPTS_DIR}/global_terraform.tfvars"
+	terraform apply "-var-file=${_SCRIPTS_DIR}/global_terraform.tfvars"
 
 	_popd
 }
 
-function _popd {
-	popd > /dev/null
-}
-
-function _pushd {
-	pushd "$1" > /dev/null
-}
-
-main "$@"
+main "${@}"
